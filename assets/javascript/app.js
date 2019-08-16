d0acb269925dcda73206b88341e0118cc6f773d7
m.peters <snakeyes721@outlook.com>
2019-08-16 12:44:15 -0700

    basic bootstrap layout and simple css spacing

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//make a start button that launches the quiz
+//set up the ____ second timer
+  //display the timer
+//set up buttons for answer choices
+  //assign a class to each button for correct and incorrect
+//register answers
+  //if correct class = true, register correct answer
+  //else register as incorrect
+  //if null then register as no answer
+//set up array for correct answers, incorrect answers and no answer
+  //track amount of elements pushed into the arrays
+//display number of elements in each array once timer expires
