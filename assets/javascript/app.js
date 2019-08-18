ae9fb53e8793a2d96422443a7b75533871fa3081
m.peters <snakeyes721@outlook.com>
2019-08-17 22:59:58 -0700

    scrapped the last one. this is all good

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,184 @@
-//make a start button that launches the quiz
-//set up the ____ second timer
-  //display the timer
-//set up buttons for answer choices
-  //assign a class to each button for correct and incorrect
-//register answers
-  //if correct class = true, register correct answer
-  //else register as incorrect
-  //if null then register as no answer
-//set up array for correct answers, incorrect answers and no answer
-  //track amount of elements pushed into the arrays
-//display number of elements in each array once timer expires
+$(document).ready(function(){
+  
+  // event listeners
+  $("#remaining-time").hide();
+  $("#start").on('click', trivia.startGame);
+  $(document).on('click' , '.option', trivia.guessChecker);
+  
+})
+
+var trivia = {
+  // trivia properties
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+  currentSet: 0,
+  timer: 20,
+  timerOn: false,
+  timerId : '',
+  // questions options and answers data
+  questions: {
+    q1: 'Which driver has the most Championships?',
+    q2: 'How many drivers are there in F1?',
+    q3: 'Which team is the only American team?',
+    q4: 'What is the average heart rate of a driver during a race?',
+    q5: "Who is the reigning World Champion?",
+    q6: 'What is the record for the fastest pit stop in F1 history?',
+    q7: "Who is the youngest driver to start a World Championship race?"
+  },
+  options: {
+    q1: ['Michael Schumacher', 'Ayrton Senna', 'Niki Lauda', 'Lewis Hamilton'],
+    q2: ['10', '20', '30', '40'],
+    q3: ['Red Bull', 'Torro Rosso', 'Racing Point', 'Haas'],
+    q4: ['60bpm', '90bpm', '120bpm', '180bpm'],
+    q5: ['Lewis Hamilton','Kimi Räikkönen','Sebastian Vettel','Kevin Magnussen'],
+    q6: ['1.89s','1.91s','1.93s','1.95s'],
+    q7: ['Charles Leclerc', 'Valtteri Bottas', 'Max Verstappen','George Russell']
+  },
+  answers: {
+    q1: 'Michael Schumacher',
+    q2: '20',
+    q3: 'Haas',
+    q4: '180bpm',
+    q5: 'Lewis Hamilton',
+    q6: '1.91s',
+    q7: 'Max Verstappen'
+  },
+  // trivia methods
+  // method to initialize game
+  startGame: function(){
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+    
+    // show game section
+    $('#game').show();
+    
+    //  empty last results
+    $('#results').html('');
+    
+    // show timer
+    $('#timer').text(trivia.timer);
+    
+    // remove start button
+    $('#start').hide();
+
+    $('#remaining-time').show();
+    
+    // ask first question
+    trivia.nextQuestion();
+    
+  },
+  // method to loop through and display questions and options 
+  nextQuestion : function(){
+    
+    // set timer to 20 seconds each question
+    trivia.timer = 10;
+     $('#timer').removeClass('last-seconds');
+    $('#timer').text(trivia.timer);
+    
+    // to prevent timer speed up
+    if(!trivia.timerOn){
+      trivia.timerId = setInterval(trivia.timerRunning, 1000);
+    }
+    
+    // gets all the questions then indexes the current questions
+    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $('#question').text(questionContent);
+    
+    // an array of all the user options for the current question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
+    // creates all the trivia guess options in the html
+    $.each(questionOptions, function(index, key){
+      $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+    })
+    
+  },
+  // method to decrement counter and count unanswered if timer runs out
+  timerRunning : function(){
+    // if timer still has time left and there are still questions left to ask
+    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+        if(trivia.timer === 4){
+          $('#timer').addClass('last-seconds');
+        }
+    }
+    // the time has run out and increment unanswered, run result
+    else if(trivia.timer === -1){
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+    }
+    // if all the questions have been shown end the game, show results
+    else if(trivia.currentSet === Object.keys(trivia.questions).length){
+      
+      // adds results of game (correct, incorrect, unanswered) to the page
+      $('#results')
+        .html("<h3>That's the checkered flag!</h3>"+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+        '<p>Get back in the seat!</p>');
+      
+      // hide game sction
+      $('#game').hide();
+      
+      // show start button to begin a new game
+      $('#start').show();
+    }
+    
+  },
+  // method to evaluate the option clicked
+  guessChecker : function() {
+    
+    // timer ID for gameResult setTimeout
+    var resultId;
+    
+    // the answer to the current question being asked
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+    // if the text of the option picked matches the answer of the current question, increment correct
+    if($(this).text() === currentAnswer){
+      // turn button green for correct
+      $(this).addClass('btn-success').removeClass('btn-info');
+      
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Correct Answer!</h3>');
+    }
+    // else the user picked the wrong option, increment incorrect
+    else{
+      // turn button clicked red for incorrect
+      $(this).addClass('btn-danger').removeClass('btn-info');
+      
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+    }
+    
+  },
+  // method to remove previous question results and options
+  guessResult : function(){
+    
+    // increment to next question set
+    trivia.currentSet++;
+    
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
+    
+    // begin next question
+    trivia.nextQuestion();
+     
+  }
+
+}
